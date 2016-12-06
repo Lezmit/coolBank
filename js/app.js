a1597a751a6c7ca8e5d2000c7456735672140078
aifos710 <sofia.m.caballero@gmail.com>
2016-12-06 18:18:19 -0500

    js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 var telefonos = new Array();
 
 $(document).ready(function(){
-	$("#next").click(validandoAporte);
-	$("#agregar-pata").click(agregarPata);
 
-	function validandoAporte() {
-		var longitud = $("#aporte").val().length;
-		if (longitud <= 5 && longitud >= 2) {
-			$("#next").attr("href", "nueva-junta-paso3.html");
-		} else {
-			alert("Puedes aportar entre 10 y 99999") 
-		}
-	}
+	var fechaSeleccionada = localStorage.getItem("fecha-inicio");
+	var total = localStorage.getItem("num-invitados")*localStorage.getItem("aporte");
+
+	$("#agregar-pata").click(agregarPata);
+	$("#btn-fecha").click(seleccionarFecha);
+	$("#aqui-fecha").text(fechaSeleccionada);
+	$("#total").text(total);
+	$("#btn-aporte").click(paso2);
+	$("#btn-confirmacion").click(paso3);
 
 	function agregarPata(){
 		var telefono = prompt("¿Cuál es el teléfono de tu pata?");
@@ -22,6 +21,30 @@ $(document).ready(function(){
 		    
 		    $("#invitados").append(template);
 		}	
+	}
+
+	function seleccionarFecha(){
+		var select = $(".nueva-junta option:selected").next().text();
+	   	localStorage.setItem("fecha-inicio", select);
+	}
+
+	function paso2(){
+		var longitud = $("#aporte").val().length;
+		if (longitud <= 5 && longitud >= 2) {
+			$("#btn-aporte").attr("href", "nueva-junta-paso3.html");
+
+			var text = $("#aporte").val();
+		   	localStorage.setItem("aporte", text);
+
+		} else {
+			alert("Puedes aportar entre 10 y 99999");
+		}
+	}
+
+	function paso3(){
+		var numInvitados = telefonos.length+1;
+
+		localStorage.setItem("num-invitados", numInvitados);
 	}
 
 })
