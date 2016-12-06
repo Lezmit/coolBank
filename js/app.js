afa44d5dd971a15bc3b1d31784026f89bc50efa9
aifos710 <sofia.m.caballero@gmail.com>
2016-12-06 15:12:42 -0500

    agregando telefonos js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var telefonos = new Array();
+
 $(document).ready(function(){
 	$("#next").click(validandoAporte);
 	$("#agregar-pata").click(agregarPata);
@@ -9,6 +11,17 @@ $(document).ready(function(){
 		} else {
 			alert("Puedes aportar entre 10 y 99999") 
 		}
+	}
+
+	function agregarPata(){
+		var telefono = prompt("¿Cuál es el teléfono de tu pata?");
+		
+		if (telefono != null) {
+			telefonos.push(telefono);
+			var template='<div class="num-personal-junta"><span class="text-junta-left">Pata</span><input type="number" value="'+telefono+'" class="text-junta-right"></input></div>';
+		    
+		    $("#invitados").append(template);
+		}	
 	}
 
 })
